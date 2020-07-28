ba18963db5788aeedc4965f92172818ff452b100
joe02 <joe02araujo@gmail.com>
2020-07-28 10:35:12 -0300

    Added ML Vision instance and TODO for future utilization.

--- lib/pages/gallery_recognition_page.dart
+++ lib/pages/gallery_recognition_page.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
@@ -12,7 +13,10 @@ class GalleryRecognition extends StatefulWidget {
 class GalleryRecognitionState extends State<GalleryRecognition> {
   var picker = ImagePicker();
   var imageFile;
-  var _image;
+  File _image;
+  var containsFile = false;
+
+  final labeler = FirebaseVision.instance.imageLabeler(ImageLabelerOptions(confidenceThreshold: 0.90));
 
   @override
   void initState() {
@@ -24,23 +28,22 @@ class GalleryRecognitionState extends State<GalleryRecognition> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: _image != null
-            ? Image.file(_image)
-            : Text("Nenhuma imagem selecionada"),
+        body: containsFile
+            ? Container(width: 200, height: 200, child: Image.file(_image))
+            : Center(child: Text("Nenhuma imagem selecionada")),
       ),
     );
   }
 
   Future openCamera() async {
-    try {
-      setState(() async {
-        imageFile = await picker.getImage(source: ImageSource.gallery);
-        setState(() {
-          _image = imageFile;
-        });
-      });
-    } catch (e) {
-      print(e);
-    }
+    imageFile = await picker.getImage(source: ImageSource.camera);
+    setState(() {
+      _image = imageFile;
+      containsFile = true;
+
+      // List<ImageLabel> labels = labeler.processImage(/*TODO Add Vision image here */);
+      // print( labels[0].text )
+      //TODO Do action considering labels[0].text as a parameter.
+    });
   }
 }
